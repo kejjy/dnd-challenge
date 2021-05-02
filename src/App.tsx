012ca57b0f05f3303348740648ad240e3a36a333
Kelly <kcocallaghan@gmail.com>
2021-05-02 17:38:04 -0500

    Implement right click functionality

--- src/App.tsx
+++ src/App.tsx
@@ -35,15 +35,14 @@ function App() {
     return 'off';
   }
 
-  function togglePathOne(stepName: string): void {
-    const togglingOff = pathOne[stepName];
-    const togglingOn = !togglingOff;
+  function togglePathOne(e: any, stepName: string, toggleOn: boolean): void {
+    e.preventDefault();
 
-    if (isPointLimitHit(togglingOn, togglingOff)) {
+    if (isPointLimitHit(toggleOn)) {
       return;
     }
 
-    if (togglingOff && canToggleOff(pathTwo, stepName)) {
+    if (!toggleOn && canToggleOff(pathOne, stepName)) {
       if (canToggleOff(pathOne, stepName)) {
         setPathOne({
           ...pathOne,
@@ -52,7 +51,7 @@ function App() {
 
         incrementPoints();
       }
-    } else if (togglingOn && canToggleOn(pathOne, stepName)) {
+    } else if (toggleOn && canToggleOn(pathOne, stepName)) {
       setPathOne({
         ...pathOne,
         [stepName]: true,
@@ -61,22 +60,23 @@ function App() {
     }
   }
 
-  function togglePathTwo(stepName: string): void {
-    const togglingOff = pathTwo[stepName];
-    const togglingOn = !togglingOff;
+  function togglePathTwo(e: any, stepName: string, toggleOn: boolean): void {
+    e.preventDefault();
 
-    if (isPointLimitHit(togglingOn, togglingOff)) {
+    if (isPointLimitHit(toggleOn)) {
       return;
     }
 
-    if (togglingOff && canToggleOff(pathTwo, stepName)) {
-      setPathTwo({
-        ...pathTwo,
-        [stepName]: false,
-      });
+    if (!toggleOn && canToggleOff(pathTwo, stepName)) {
+      if (canToggleOff(pathTwo, stepName)) {
+        setPathTwo({
+          ...pathTwo,
+          [stepName]: false,
+        });
 
-      incrementPoints();
-    } else if (togglingOn && canToggleOn(pathTwo, stepName)) {
+        incrementPoints();
+      }
+    } else if (toggleOn && canToggleOn(pathTwo, stepName)) {
       setPathTwo({
         ...pathTwo,
         [stepName]: true,
@@ -93,8 +93,8 @@ function App() {
     setPoints(points - 1);
   }
 
-  function isPointLimitHit(togglingOn: boolean, togglingOff: boolean) {
-    return (points === 0 && togglingOn) || (points === 6 && togglingOff);
+  function isPointLimitHit(togglingOn: boolean) {
+    return (points === 0 && togglingOn) || (points === 6 && !togglingOn);
   }
 
   function canToggleOn(path: Path, stepName: string): boolean {
@@ -117,44 +117,52 @@ function App() {
           <div className="path-name">TALENT PATH 1</div>
           <div
             className={`sprite-icon sprite-icon-boxes ${getStepOnOffClass(pathOne, 'stepOne')}`}
-            onClick={() => togglePathOne('stepOne')}
+            onClick={(e) => togglePathOne(e, 'stepOne', true)}
+            onContextMenu={(e) => togglePathOne(e, 'stepOne', false)}
           ></div>
           <div className={`divider ${getDividerOnOffClass(pathOne, 'stepOne')}`}></div>
           <div
             className={`sprite-icon sprite-icon-silverware ${getStepOnOffClass(pathOne, 'stepTwo')}`}
-            onClick={() => togglePathOne('stepTwo')}
+            onClick={(e) => togglePathOne(e, 'stepTwo', true)}
+            onContextMenu={(e) => togglePathOne(e, 'stepTwo', false)}
           ></div>
           <div className={`divider ${getDividerOnOffClass(pathOne, 'stepTwo')}`}></div>
           <div
             className={`sprite-icon sprite-icon-cake ${getStepOnOffClass(pathOne, 'stepThree')}`}
-            onClick={() => togglePathOne('stepThree')}
+            onClick={(e) => togglePathOne(e, 'stepThree', true)}
+            onContextMenu={(e) => togglePathOne(e, 'stepThree', false)}
           ></div>
           <div className={`divider ${getDividerOnOffClass(pathOne, 'stepThree')}`}></div>
           <div
             className={`sprite-icon sprite-icon-crown ${getStepOnOffClass(pathOne, 'stepFour')}`}
-            onClick={() => togglePathOne('stepFour')}
+            onClick={(e) => togglePathOne(e, 'stepFour', true)}
+            onContextMenu={(e) => togglePathOne(e, 'stepFour', false)}
           ></div>
         </div>
         <div className="path-two">
           <div className="path-name">TALENT PATH 2</div>
           <div
             className={`sprite-icon sprite-icon-boat ${getStepOnOffClass(pathTwo, 'stepOne')}`}
-            onClick={() => togglePathTwo('stepOne')}
+            onClick={(e) => togglePathTwo(e, 'stepOne', true)}
+            onContextMenu={(e) => togglePathTwo(e, 'stepOne', false)}
           ></div>
           <div className={`divider ${getDividerOnOffClass(pathTwo, 'stepOne')}`}></div>
           <div
             className={`sprite-icon sprite-icon-snorkel ${getStepOnOffClass(pathTwo, 'stepTwo')}`}
-            onClick={() => togglePathTwo('stepTwo')}
+            onClick={(e) => togglePathTwo(e, 'stepTwo', true)}
+            onContextMenu={(e) => togglePathTwo(e, 'stepTwo', false)}
           ></div>
           <div className={`divider ${getDividerOnOffClass(pathTwo, 'stepTwo')}`}></div>
           <div
             className={`sprite-icon sprite-icon-lightning ${getStepOnOffClass(pathTwo, 'stepThree')}`}
-            onClick={() => togglePathTwo('stepThree')}
+            onClick={(e) => togglePathTwo(e, 'stepThree', true)}
+            onContextMenu={(e) => togglePathTwo(e, 'stepThree', false)}
           ></div>
           <div className={`divider ${getDividerOnOffClass(pathTwo, 'stepThree')}`}></div>
           <div
             className={`sprite-icon sprite-icon-skull ${getStepOnOffClass(pathTwo, 'stepFour')}`}
-            onClick={() => togglePathTwo('stepFour')}
+            onClick={(e) => togglePathTwo(e, 'stepFour', true)}
+            onContextMenu={(e) => togglePathTwo(e, 'stepFour', false)}
           ></div>
         </div>
       </div>
