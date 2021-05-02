ac3b9f44b5aaaf4748e5f893ed67637cf4045e3a
Kelly <kcocallaghan@gmail.com>
2021-05-02 16:21:20 -0500

    Flexin

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,26 @@ function App() {
         <span>TitanStar Legends - Rune Master Loadout Talent Calculator 9000</span>
       </header>
       <div className="path-container">
-        <div className="sprite-icon sprite-icon-boxes"></div>
-        <div className="sprite-icon sprite-icon-skull"></div>
-        <div className="sprite-icon sprite-icon-lightning"></div>
-        <div className="sprite-icon sprite-icon-snorkel"></div>
-        <div className="sprite-icon sprite-icon-boat"></div>
-        <div className="sprite-icon sprite-icon-crown"></div>
-        <div className="sprite-icon sprite-icon-cake"></div>
-        <div className="sprite-icon sprite-icon-silverware"></div>
+        <div className="path-one">
+          <div className="path-name">TALENT PATH 1</div>
+          <div className="sprite-icon sprite-icon-boxes"></div>
+          <div className="divider"></div>
+          <div className="sprite-icon sprite-icon-silverware"></div>
+          <div className="divider"></div>
+          <div className="sprite-icon sprite-icon-cake"></div>
+          <div className="divider"></div>
+          <div className="sprite-icon sprite-icon-crown"></div>
+        </div>
+        <div className="path-two">
+          <div className="path-name">TALENT PATH 2</div>
+          <div className="sprite-icon sprite-icon-boat"></div>
+          <div className="divider"></div>
+          <div className="sprite-icon sprite-icon-snorkel"></div>
+          <div className="divider"></div>
+          <div className="sprite-icon sprite-icon-lightning"></div>
+          <div className="divider"></div>
+          <div className="sprite-icon sprite-icon-skull"></div>
+        </div>
       </div>
     </div>
   );
