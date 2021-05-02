e630911400c9022b2653877548e7b7f380ed094d
Kelly <kcocallaghan@gmail.com>
2021-05-02 16:01:48 -0500

    Getting started with sprites

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <span>TitanStar Legends - Rune Master Loadout Talent Calculator 9000</span>
+      </header>
+      <div className="path-container">
+        <div className="sprite-icon sprite-icon-boxes"></div>
+        <div className="sprite-icon sprite-icon-skull"></div>
+        <div className="sprite-icon sprite-icon-lightning"></div>
+        <div className="sprite-icon sprite-icon-snorkel"></div>
+        <div className="sprite-icon sprite-icon-boat"></div>
+        <div className="sprite-icon sprite-icon-crown"></div>
+        <div className="sprite-icon sprite-icon-cake"></div>
+        <div className="sprite-icon sprite-icon-silverware"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
